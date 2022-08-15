d121e36827da0218d116538d29cb404286dfdeec
Miguel Figueiredo <miguelpfigueiredo@gmail.com>
2022-08-15 17:42:40 +0100

    Simple AirBnB

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,21 @@
+require "open-uri"
+
+class FlatsController < ApplicationController
+  before_action :set_flats
+  
+  def index
+  end
+
+  def show
+    flat_id = params[:id].to_i
+
+    @flat = @flats.find { |flat| flat["id"] == flat_id }
+  end
+
+  private
+
+  def set_flats
+    url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
+    @flats = JSON.parse(URI.open(url).read) 
+  end
+end
